7bfa291e084c3fb0394d3ae6d889fccd1e3de7e4
Minh Duc <tmduc30697@gmail.com>
2024-12-24 22:36:13 +0700

    :+1: Setup vuetify

--- app.vue
+++ app.vue
@@ -1,1 +1,7 @@
-<template></template>
+<template>
+  <NuxtLayout>
+    <v-app>
+      <NuxtPage />
+    </v-app>
+  </NuxtLayout>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,24 @@
+import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
+
 export default defineNuxtConfig({
   compatibilityDate: "2024-12-24",
   devtools: { enabled: true },
+  build: {
+    transpile: ["vuetify"],
+  },
+  modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook("vite:extendConfig", (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }));
+      });
+    },
+  ],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
 });
