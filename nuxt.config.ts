ab2e9f65490233027902bb69e39c789ecf96b03b
Minh Duc <tmduc30697@gmail.com>
2024-12-24 22:58:41 +0700

    :+1: Setup tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,4 +21,11 @@ export default defineNuxtConfig({
       },
     },
   },
+  css: ["~/assets/css/main.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 });
